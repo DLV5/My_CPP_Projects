67926e5a39e6ce8ae62a81e92a2707ca3081ddc4
Kameji <diman567tak@gmail.com>
2024-10-16 12:00:26 +0300

    Added simple calculator class

--- MyCPP_01/MyCPP_01/main.cpp
+++ MyCPP_01/MyCPP_01/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include "Calculator.h"
 
 int main() {
-	std::cout << "MyCPP_01: Hello world!";
+	std::cout << "MyCPP_01: Hello world! \n";
+
+	Calculator Calculator;
+	Calculator.Add(1.0f, 2.0f);
+	Calculator.Subtract(10.0f, 5.0f);
 	return 0;
 }
